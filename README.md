5e79aecec69f92bc621fa5980e6bec65eb679160
ivanheral <ivanhernandezal@gmail.com>
2019-04-29 21:57:10 +0200

    add servor

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # todo-list-Marvel
-Only Chrome.
+
+Search - Marvel Characters, Super Heroes, & Villains
+
+## Install
+
+Install with npm or Yarn.
+
+<pre>
+npm dev
+</pre>
--- taskfile.js
+++ taskfile.js
@@ -1,26 +1,20 @@
-var bs = require('browser-sync')
-var historyApiFallback = require('connect-history-api-fallback')
-var env = 'development'
-var fw = 'preact'
-var out = 'dist'
+import bs from 'servor';
+bs.start({
+    root: 'dist',
+    port: 9000
+});
 
-bs({
-    server: `${out}`,
-    middleware: [historyApiFallback()]
-})
+var env = 'development';
+var fw = 'preact';
+var out = 'dist';
 
 export default async function (task) {
     await task.parallel(['js', 'html'])
     await task.watch('src/js/**/*.*', 'js')
-    await task.watch(`${out}/**/*.js`, 'changes')
 }
 
 export async function html(task) {
     await task.source('src/html/index.html').target(`${out}`)
-}
-
-export async function changes(task) {
-    bs.reload()
 }
 
 export async function js(task) {
